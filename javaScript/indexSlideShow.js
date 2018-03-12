01923d9be7270b2a812b73eb4d082eb0ab60bbcf
Dcorum <dpcorum@yahoo.com>
2018-03-12 17:21:26 -0600

    updated slideshow to include a timer

---
+++ javaScript/indexSlideShow.js
@@ -0,0 +1,16 @@
+var slideArr = ["./Pictures/raccooncity1.jpg", "./Pictures/raccooncity2.jpg", "./Pictures/raccooncity3.jpg"]
+var slideContainer = document.getElementById("slideshow");
+var imageIndex = 0;
+
+function changeImage() {
+    document.getElementById("slideshow").innerHTML = "<img src="+slideArr[imageIndex]+ "/>"; 
+    imageIndex++;
+    if (imageIndex >= slideArr.length) {
+        imageIndex = 0;
+    }
+}
+window.onload = function() {
+ 
+   setInterval(changeImage,2500);
+ 
+}
